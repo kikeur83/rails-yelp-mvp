34d56ab055242b3ab1b8278013cdd1917c45e9bc
cd <charlydubostbp@hotmail.fr>
2022-06-01 17:01:23 +0200

    add controller restaurants + show index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+puts "creating"
+Restaurant.create([{ name: 'chez shishi', address: '27 rue de la truite', category: 'japanese', phone_number: "0654897825" }, {name: 'frite une fois', address: '12 boulevard de la patate', category: 'belgian', phone_number: "0654893527" }, { name: 'pizza maquer', address: '11 rue yolo', category: 'italian', phone_number: "0674893528"}, { name: 'micro-nem', address: '69 avenue de la petite baguette', category: 'chinese', phone_number: "0653893528" }, { name: 'la tour', address: '122 boulevard effel', category: 'french', phone_number: "0654893428"}])
+puts "finish"
